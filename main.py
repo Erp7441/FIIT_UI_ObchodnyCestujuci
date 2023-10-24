b7e163e3363d61b26be08dab2c26f6acbcfcf8d3
Martin Szabo <martin.szabo333@gmail.com>
2023-10-24 16:09:51 +0200

    Project setup

---
+++ main.py
@@ -0,0 +1,21 @@
+# Fitnes funkcia - Ohodnoti clena populacie nejakym indexom
+
+# Pseudo kod GA
+# 0. Start
+# 1. Fitnes (vyhodnotenie kazdeho chromozomu v populacii)
+# 2. Reprodukcia
+    # 2.0 Vyber (Zalozeny na fitnes value)
+    # 2.1 Rekombinacia (Krizenie chromozomov)
+    # 2.2 Mutacia (Mutacia chromozomov (na zakladne nejakej pravdepodobnostnej value))
+    # 2.3 Prijatie (Remove alebo accept novy chromozom)
+# 3. Nahrada (Nahradenie starej populacie novou)
+# 4. Test (Testovanie ci sa mi riesenie nachadza v novej populacii)
+# 5. Loop (Opakuj 1-4 dokym nejaky chromozom prejde uspesne testom [diskutabilne])
+
+# Problemy GA
+# 1. Metoda kodovania
+# 2. Procedura incializacie
+# 3. Vyhodnocovanie fitnes
+# 4. Vyber rodicov
+# 5. Geneticke operatory?
+# 6. Nastavenie parametrov
---
+++ utils/Params.py
@@ -0,0 +1,4 @@
+class Params:
+    def __new__(cls):
+        # Throw error if class is instantiated
+        pass
