c5722148e30082670c2c810a318334aaeeeda8eb
Martin Szabo <martin.szabo333@gmail.com>
2023-11-01 21:33:59 +0100

    Refactoring timer

--- utils/Timer.py
+++ utils/Timer.py
@@ -3,11 +3,10 @@ from utils.Constants import ROUNDING_PRECISION
 
 
 class Timer:
-    def __init__(self, precision=3):
+    def __init__(self):
         self.start_time = None
         self.end_time = None
         self.elapsed_time = None
-        self.precision = ROUNDING_PRECISION
 
     def start(self):
         self.start_time = time()
@@ -15,4 +14,4 @@ class Timer:
     def stop(self):
         self.end_time = time()
         if self.start_time is not None and self.end_time is not None:
-            self.elapsed_time = round(self.end_time - self.start_time, self.precision)
+            self.elapsed_time = round(self.end_time - self.start_time, ROUNDING_PRECISION)
